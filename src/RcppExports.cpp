3ce1db283b4f7a09d3c7e53fb68e86006ddfb455
Jason Cory Brunson <cornelioid@gmail.com>
2017-11-16 18:12:56 -0500

    MFR Algorithm 1 up until std::map stuff

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,18 +6,20 @@
 using namespace Rcpp;
 
 // mfrs_dag
-List mfrs_dag();
-RcppExport SEXP _rmfr_mfrs_dag() {
+List mfrs_dag(int node_count, IntegerVector composite_nodes);
+RcppExport SEXP _rmfr_mfrs_dag(SEXP node_countSEXP, SEXP composite_nodesSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
-    rcpp_result_gen = Rcpp::wrap(mfrs_dag());
+    Rcpp::traits::input_parameter< int >::type node_count(node_countSEXP);
+    Rcpp::traits::input_parameter< IntegerVector >::type composite_nodes(composite_nodesSEXP);
+    rcpp_result_gen = Rcpp::wrap(mfrs_dag(node_count, composite_nodes));
     return rcpp_result_gen;
 END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_rmfr_mfrs_dag", (DL_FUNC) &_rmfr_mfrs_dag, 0},
+    {"_rmfr_mfrs_dag", (DL_FUNC) &_rmfr_mfrs_dag, 2},
     {NULL, NULL, 0}
 };
 
--- src/mfr-algorithms.cpp
+++ src/mfr-algorithms.cpp
@@ -1,17 +1,36 @@
 #include <Rcpp.h>
-//#include "mfrs.h"
+#include <set>
+#include <stack>
+#include <map>
+#include "mfrs.h"
 using namespace Rcpp;
 
 // Algorithm 1 (Wang et al, 2013)
 // A depth-first search algorithm for enumerating MFRs in DAGs
 // [[Rcpp::export]]
-List mfrs_dag() {
+List mfrs_dag(int node_count, IntegerVector composite_nodes) {
   
   // initialization
   
   int mfr_count = 0; // "count"
   std::set< std::vector<int> > mfr_set; // "MFR"
-  //std::stack<int> link_stack; // "edge_list"
+  std::stack<int> link_stack; // "edge_list"
+  
+  // "visit_num([node])"
+  std::map<int,int> node_visit_count;
+  for (int i = 0; i < node_count; i++) node_visit_count[i] = 0;
+  // "partialMFR([node])"
+  std::map<int, std::set< std::vector<int> > > node_partial_mfr_map;
+  
+  std::map<int,int> link_visit_count; // "visit_num([link])"
+  for (int i = 0; i < node_count; i++) link_visit_count[i] = 0;
+  // "partialMFR([link])"
+  std::map<int, std::set< std::vector<int> > > clink_partial_mfr_map;
+  for (int i = 0; i < composite_nodes.size(); i++) {
+    //clink_partial_mfr_map[composite_nodes[i]] = 0;
+  }
+  
+  // ... more stuff ...
   
   // return list
   return List::create(
