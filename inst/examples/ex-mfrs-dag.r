da62e02a25cefae69045a05de00b6dedef2f7cee
Jason Cory Brunson <cornelioid@gmail.com>
2017-11-29 17:22:11 -0500

    debugging Alg. 1

--- inst/examples/ex-mfrs-dag.r
+++ inst/examples/ex-mfrs-dag.r
@@ -1,12 +1,16 @@
 # acyclic example
 g <- example_acyclic
 h <- expand_graph(g)
+plot(h,
+     layout = layout_as_tree,
+     vertex.color = ifelse(V(h)$composite, "gray", "white"),
+     vertex.label = as.numeric(V(h)))
 \dontrun{
 g_mfrs <- mfrs_dag(
   node_count = vcount(h),
   link_array = as_edgelist(h, names = FALSE),
   composite_nodes = which(vertex_attr(h, "composite")),
-  source_node = which(vertex_attr(h, "name") == "s"),
-  target_node = which(vertex_attr(h, "name") == "t")
+  source_node = 1,
+  target_node = 10
 )
 }
--- src/mfr-algorithms.cpp
+++ src/mfr-algorithms.cpp
@@ -33,28 +33,26 @@ List mfrs_dag(int node_count,
   std::stack<int> link_stack; // "edge_list"
   
   // "visit_num([node])"
-  //std::map<int,int> node_visit_count;
-  //int node_visit_count[node_count];
-  std::vector<int> node_visit_count;
+  int node_visit_count[node_count];
   // "partialMFR([node])"
   std::map<int, route_set> node_pmfr_map;
   // initialize maps and fill in-link vectors
   for (int i = 0; i < node_count; i++) {
-    //node_visit_count[i] = 0;
-    node_visit_count.push_back(0);
-    node_pmfr_map[i];
-    //for (int j = 0; j < link_array.nrow(); j++) {
-    //  if (link_array(j, 1) == i) {
-    //    composite_inlinks[i].push_back(j);
-    //  }
-    //}
+    node_visit_count[i] = 0;
+    route empty_route;
+    route_set empty_route_set;
+    empty_route_set.insert(empty_route);
+    node_pmfr_map.insert(std::map<int, route_set>::value_type(
+        i, empty_route_set
+    ));
+    Rcout << "node " << i << " pMFR size = " <<
+      node_pmfr_map[i].size() << std::endl;
   }
   
   // in-links to composite nodes
   std::map<int, std::vector<int> > composite_inlinks;
   // "visit_num([link])"
-  //std::vector< std::vector<int> > inlink_visit_counts;
-  std::map<int, std::map<int, int> > inlink_visit_counts;
+  std::map<int, std::map<int,int> > inlink_visit_counts;
   // "partialMFR([link])"
   std::map<int, std::map<int, route_set> > inlink_pmfr_maps;
   for (int i = 0; i < composite_nodes.size(); i++) {
@@ -64,29 +62,48 @@ List mfrs_dag(int node_count,
         composite_inlinks[composite_nodes[i]].push_back(j);
       }
     }
-    //std::vector<int> inlink_visit_count;
-    std::map<int, int> inlink_visit_count;
+    std::map<int,int> inlink_visit_count;
     std::map<int, route_set> inlink_pmfr_map;
-    for (int j = 0; j < composite_inlinks[i].size(); j++) {
-      //inlink_visit_count.push_back(0);
-      inlink_visit_count.insert(std::map<int, int>::value_type(
-        composite_inlinks[i][j], 0
+    for (int j = 0; j < composite_inlinks[composite_nodes[i]].size(); j++) {
+      inlink_visit_count.insert(std::map<int,int>::value_type(
+          composite_inlinks[composite_nodes[i]][j], 0
       ));
-      inlink_pmfr_map[composite_inlinks[i][j]];
+      route empty_route;
+      route_set empty_route_set;
+      empty_route_set.insert(empty_route);
+      inlink_pmfr_map.insert(std::map<int, route_set>::value_type(
+          composite_inlinks[composite_nodes[i]][j], empty_route_set
+      ));
+      Rcout << "in-link " << composite_inlinks[composite_nodes[i]][j] <<
+        " pMFR size = " <<
+          inlink_pmfr_map[composite_inlinks[composite_nodes[i]][j]].size() <<
+            std::endl;
     }
-    //inlink_visit_counts.push_back(inlink_visit_count);
-    inlink_visit_counts.insert(std::map<int, std::map<int, int> >::value_type(
-      composite_nodes[i], inlink_visit_count
+    inlink_visit_counts.insert(std::map<int, std::map<int,int> >::value_type(
+        composite_nodes[i], inlink_visit_count
     ));
-    //inlink_pmfr_maps.push_back(inlink_pmfr_map);
     inlink_pmfr_maps.insert(std::map<int, std::map<int, route_set> >::value_type(
-      composite_nodes[i], inlink_pmfr_map
+        composite_nodes[i], inlink_pmfr_map
     ));
+  }
+  
+  if (0) {
+    Rcout << "in-link visit counts:" << std::endl;
+    for (int i = 0; i < composite_nodes.size(); i++) {
+      int cn = composite_nodes[i];
+      for (std::map<int,int>::iterator it = inlink_visit_counts[cn].begin();
+           it != inlink_visit_counts[cn].end();
+           ++it) {
+        Rcout << it->first << " => " << it->second << std::endl;
+      }
+    }
   }
   
   // visiting the source node
   
-  node_visit_count[source_node] = 1;
+  // set 'source_node' visit count to 1
+  node_visit_count[source_node - 1] = 1;
+  // append to 'link_stack' each link from 'source_node'
   for (int i = 0; i < link_array.nrow(); i++) {
     if (link_array(i, 0) == source_node) {
       link_stack.push(i);
@@ -97,11 +114,31 @@ List mfrs_dag(int node_count,
   
   while (link_stack.size() > 0) {
     
+    if (0) {
+      //Rcout << link_stack << std::endl;
+      Rcout << "link stack: ";
+      std::stack<int> temp;
+      temp = link_stack;
+      while (!temp.empty()) {
+        int w = temp.top();
+        Rcout << w << " ";
+        temp.pop();
+      }
+      Rcout << std::endl;
+      
+      Rcout << "node visit counts: ";
+      for (int i = 0; i < node_count; i++) {
+        Rcout << node_visit_count[i] << " ";
+      }
+      Rcout << std::endl;
+    }
+    
     // pop out the latest link
     int e = link_stack.top();
     link_stack.pop();
     int u = link_array(e, 0);
     int w = link_array(e, 1);
+    Rcout << "link e = " << e << "; nodes u = " << u << ", w = " << w << std::endl;
     
     // is 'w' a composite node?
     int *i = std::find(std::begin(composite_nodes),
@@ -109,36 +146,73 @@ List mfrs_dag(int node_count,
                        w);
     int w_composite = (i != std::end(composite_nodes));
     // concatenate 'e' to the set of partial routes terminating at 'u'
-    route_set temp_route_set = node_pmfr_map[u];
-    //for (int j = 0; j < temp_route_set.size(); j++) {
-    //  temp_route_set[j].insert(e);
-    //}
+    //route_set temp_route_set = node_pmfr_map[u - 1];
+    route_set temp_route_set;
     route_set::iterator it;
-    for (it = temp_route_set.begin(); it != temp_route_set.end(); it++) {
-      route temp_route = *it;
-      temp_route.push_back(e);
+    Rcout << "'temp_route_set' route lengths:" << std::endl;
+    for (it = node_pmfr_map[u - 1].begin();
+         it != node_pmfr_map[u - 1].end();
+         it++) {
+      route r = *it;
+      Rcout << r.size();
+      r.push_back(e);
+      Rcout << " (" << r.size() << ") ";
+      temp_route_set.insert(r);
     }
+    Rcout << std::endl;
+    Rcout << "'temp_route_set' size: " << temp_route_set.size() << std::endl;
     
-    std::map<int, int> w_inlink_visit_count;
+    std::map<int,int> w_inlink_visit_count;
     std::map<int, route_set> w_inlink_pmfr_map;
+    
     // if 'w' is a composite node...
     if (w_composite) {
       // Temp1 and Temp2
       w_inlink_visit_count = inlink_visit_counts[w];
       w_inlink_pmfr_map = inlink_pmfr_maps[w];
       // increment the in-link visit count
-      //link_visit_count[e] = link_visit_count[e] + node_visit_count[u];
-      inlink_visit_counts[w][e] += node_visit_count[u];
+      //int we_inlink_count = inlink_visit_counts[w][e] + node_visit_count[u - 1];
+      //inlink_visit_counts[w][e] = we_inlink_count;
+      inlink_visit_counts[w][e] += node_visit_count[u - 1];
+      
       // append the concatenated routes to the appropriate route set
-      inlink_pmfr_maps[w][e].insert(temp_route_set.begin(),
-                                    temp_route_set.end());
+      route_set we_inlink_pmfrs;
+      route_set::iterator it;
+      for (it = inlink_pmfr_maps[w][e].begin();
+           it != inlink_pmfr_maps[w][e].end();
+           it++) {
+        route r = *it;
+        r.push_back(e);
+        we_inlink_pmfrs.insert(r);
+      }
+      //route_set we_inlink_pmfrs = inlink_pmfr_maps[w][e];
+      //we_inlink_pmfrs.insert(temp_route_set.begin(),
+      //                       temp_route_set.end());
+      inlink_pmfr_maps[w][e] = we_inlink_pmfrs;
+      
+      if (0) {
+        Rcout << "in-link visit counts:" << std::endl;
+        for (int i = 0; i < composite_nodes.size(); i++) {
+          int cn = composite_nodes[i];
+          for (std::map<int,int>::iterator it = inlink_visit_counts[cn].begin();
+               it != inlink_visit_counts[cn].end();
+               ++it) {
+            Rcout << it->first << " => " << it->second << std::endl;
+          }
+        }
+      }
+      
     }
     
     // if 'w' is the sink node...
     if (w == target_node) {
-      mfr_count += node_visit_count[u];
+      
+      mfr_count += node_visit_count[u - 1];
+      Rcout << "'mfr_count' = " << mfr_count << std::endl;
       mfr_set.insert(temp_route_set.begin(),
                      temp_route_set.end());
+      Rcout << "'mfr_set' size = " << mfr_set.size() << std::endl;
+      
     } else {
       
       // have all links to 'w' been visited?
@@ -150,6 +224,7 @@ List mfrs_dag(int node_count,
           }
         }
       }
+      //Rcout << "all in-links visited: " << w_inlinks_visited << std::endl;
       
       // if 'w' is original or all links to 'w' have been visited...
       if ((!w_composite) | (w_inlinks_visited)) {
@@ -163,34 +238,112 @@ List mfrs_dag(int node_count,
         
         // set the visit count and route set of 'w'
         if (!w_composite) {
-          node_visit_count[w] = node_visit_count[u];
-          // https://codeyarns.com/2011/01/11/c-insert-or-update-a-map/
-          node_pmfr_map[w] = temp_route_set;
+          
+          // reset 'partialMFR' at 'w' to the concatenated 'partialMFR' at 'u'
+          node_visit_count[w - 1] = node_visit_count[u - 1];
+          route_set w_node_pmfrs = node_pmfr_map[u - 1];
+          route_set::iterator it;
+          for (it = w_node_pmfrs.begin();
+               it != w_node_pmfrs.end();
+               it++) {
+            route r = *it;
+            r.push_back(e);
+            Rcout << "route length: " << r.size() << std::endl;
+          }
+          node_pmfr_map[w - 1] = w_node_pmfrs;
+          //node_pmfr_map[w - 1] = temp_route_set;
+          
         } else {
+          
           // visit_num(w) = Pi_v visit_num(e_{v,w}) - Pi_v Temp1(e_{v,w})
           int curr_prod = 1;
           int prev_prod = 1;
-          for (int k = 0; k < inlink_visit_counts[w].size(); k++) {
-            curr_prod *= inlink_visit_counts[w][composite_inlinks[w][k]];
-            prev_prod *= w_inlink_visit_count[composite_inlinks[w][k]];
+          for (int i = 0; i < inlink_visit_counts[w].size(); i++) {
+            curr_prod *= inlink_visit_counts[w][composite_inlinks[w][i]];
+            prev_prod *= w_inlink_visit_count[composite_inlinks[w][i]];
           }
-          node_visit_count[w] = curr_prod - prev_prod;
+          node_visit_count[w - 1] = curr_prod - prev_prod;
+          Rcout << "product difference: " << curr_prod << " - " <<
+            prev_prod << " = " << node_visit_count[w - 1] << std::endl;
+          
           // set-difference:
-          // partialMFR(w) = Otimes_v partialMFR(e_{v,w}) - Otimes_v Temp2(e_{v,w})
+          // pMFR(w) = Otimes_v pMFR(e_{v,w}) - Otimes_v Temp2(e_{v,w})
+          
+          // combine pairs of PMFRs, one through each link to 'w'
+          // NEED TO ALLOW FOR MORE THAN TWO IN-LINKS
+          route_set::iterator it0;
+          route_set::iterator it1;
           route_set curr_combn;
-          route_set prev_combn;
-          for (int k = 0; k < inlink_pmfr_maps[w].size(); k++) {
-            curr_combn.insert(inlink_pmfr_maps[w][composite_inlinks[w][k]].begin(),
-                              inlink_pmfr_maps[w][composite_inlinks[w][k]].end());
-            prev_combn.insert(w_inlink_pmfr_map[composite_inlinks[w][k]].begin(),
-                              w_inlink_pmfr_map[composite_inlinks[w][k]].end());
+          for (it0 = inlink_pmfr_maps[w][composite_inlinks[w][0]].begin();
+               it0 != inlink_pmfr_maps[w][composite_inlinks[w][0]].end();
+               it0++) {
+            route r0 = *it0;
+            for (it1 = inlink_pmfr_maps[w][composite_inlinks[w][1]].begin();
+                 it1 != inlink_pmfr_maps[w][composite_inlinks[w][1]].end();
+                 it1++) {
+              route r1 = *it1;
+              route r = r0;
+              r.insert(r.end(), r1.begin(), r1.end());
+              std::sort(r.begin(), r.end());
+              curr_combn.insert(r);
+              Rcout << "add to 'curr_combn': ";
+              for (int i = 0; i < r.size(); i++) {
+                Rcout << r[i] << " ";
+              }
+              Rcout << std::endl;
+            }
           }
-          //node_pmfr_map[w] = std::set_difference();
-          node_pmfr_map[w] = curr_combn;
+          route_set prev_combn;
+          for (it0 = w_inlink_pmfr_map[composite_inlinks[w][0]].begin();
+               it0 != w_inlink_pmfr_map[composite_inlinks[w][0]].end();
+               it0++) {
+            route r0 = *it0;
+            for (it1 = w_inlink_pmfr_map[composite_inlinks[w][1]].begin();
+                 it1 != w_inlink_pmfr_map[composite_inlinks[w][1]].end();
+                 it1++) {
+              route r1 = *it1;
+              route r = r0;
+              r.insert(r.end(), r1.begin(), r1.end());
+              std::sort(r.begin(), r.end());
+              prev_combn.insert(r);
+              Rcout << "add to 'prev_combn': ";
+              for (int i = 0; i < r.size(); i++) {
+                Rcout << r[i] << " ";
+              }
+              Rcout << std::endl;
+            }
+          }
+          if (0) {
+            route_set curr_combn;
+            route_set prev_combn;
+            for (int i = 0; i < inlink_pmfr_maps[w].size(); i++) {
+              curr_combn.insert(
+                inlink_pmfr_maps[w][composite_inlinks[w][i]].begin(),
+                inlink_pmfr_maps[w][composite_inlinks[w][i]].end()
+              );
+              prev_combn.insert(
+                w_inlink_pmfr_map[composite_inlinks[w][i]].begin(),
+                w_inlink_pmfr_map[composite_inlinks[w][i]].end()
+              );
+            }
+          }
+          Rcout << "current route set: " << curr_combn.size() << std::endl;
+          Rcout << "previous route set: " << prev_combn.size() << std::endl;
           route_set::iterator it;
           for (it = prev_combn.begin(); it != prev_combn.end(); it++) {
-            route prev_route = *it;
-            node_pmfr_map[w].erase(prev_route);
+            route r = *it;
+            curr_combn.erase(r);
+          }
+          node_pmfr_map[w - 1] = curr_combn;
+          Rcout << "new node_pmfr_map[w-1]: " << std::endl;
+          for (it = node_pmfr_map[w - 1].begin();
+               it != node_pmfr_map[w - 1].end();
+               it++) {
+            route r = *it;
+            for (int i = 0; i < r.size(); i++) {
+              Rcout << r[i] << " ";
+            }
+            Rcout << std::endl;
           }
         }
         
@@ -205,6 +358,7 @@ List mfrs_dag(int node_count,
     _["mfr_count"] = mfr_count,
     _["mfr_set"] = mfr_set
   );
+  
 }
 
 // Algorithm 2 (Wang et al, 2013)
