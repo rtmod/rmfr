c0e1e33f6f5f85b1e74ced1fc41420e207eae17e
Jason Cory Brunson <cornelioid@gmail.com>
2017-11-30 14:16:13 -0500

    example typo

--- inst/examples/ex-mfrs-dag.r
+++ inst/examples/ex-mfrs-dag.r
@@ -4,7 +4,7 @@ h <- expand_graph(g)
 plot(h,
      layout = layout_as_tree,
      vertex.color = ifelse(V(h)$composite, "gray", "white"))
-g_mfrs <- mfrs_dag(
+h_mfrs <- mfrs_dag(
   node_count = vcount(h),
   link_array = as_edgelist(h, names = FALSE),
   composite_nodes = which(vertex_attr(h, "composite")),
@@ -13,9 +13,9 @@ g_mfrs <- mfrs_dag(
 )
 
 par(mfrow = c(2, 3))
-for (i in 1:g_mfrs$mfr_count) {
+for (i in 1:h_mfrs$mfr_count) {
   h <- set_edge_attr(h, "MFR",
-                     value = (as.numeric(E(h)) - 1) %in% g_mfrs$mfr_set[[i]])
+                     value = (as.numeric(E(h)) - 1) %in% h_mfrs$mfr_set[[i]])
   plot(h,
        layout = layout_as_tree,
        vertex.color = ifelse(V(h)$composite, "gray", "white"),
@@ -27,9 +27,9 @@ par(mfrow = c(1, 1))
 g_ <- contract_graph(h)
 
 par(mfrow = c(2, 3))
-for (i in 1:g_mfrs$mfr_count) {
+for (i in 1:h_mfrs$mfr_count) {
   g_ <- set_edge_attr(g_, "MFR",
-                      value = (as.numeric(E(g_)) - 1) %in% g_mfrs$mfr_set[[i]])
+                      value = (as.numeric(E(g_)) - 1) %in% h_mfrs$mfr_set[[i]])
   plot(g_,
        layout = layout_as_tree,
        vertex.color = ifelse(V(g_)$composite, "gray", "white"),
